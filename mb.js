6e47960dcfdbfb8c27425363dac7d91672e5699a
Ali Muhammad <ali.munchkin08@gmail.com>
2024-01-04 18:13:33 +0500

    amogus

--- mb.js
+++ mb.js
@@ -51,7 +51,7 @@ const main = () => {
     );
     xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
-        document = xhr.responseText;
+        document.innerHTML = xhr.responseText;
       }
     };
     xhr.send();
